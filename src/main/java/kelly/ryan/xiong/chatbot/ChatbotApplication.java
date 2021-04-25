a1674de8e19eac0cb2981f22e00ba653c5a723b8
xiong <xiong@zipcodes-MBP-8.fios-router.home>
2021-04-25 19:07:11 -0400

    fixed database issue

--- src/main/java/kelly/ryan/xiong/chatbot/ChatbotApplication.java
+++ src/main/java/kelly/ryan/xiong/chatbot/ChatbotApplication.java
@@ -1,22 +1,12 @@
 package kelly.ryan.xiong.chatbot;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ChatbotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatbotApplication.class, args);
-	}
-
-	@Bean
-	ServletRegistrationBean h2servletRegistration(){
-		ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
-		registrationBean.addUrlMappings("/console/*");
-		return registrationBean;
 	}
 }
