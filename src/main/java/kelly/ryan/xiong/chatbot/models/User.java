3a554b16efabf9e41d047b85e024923ed7fd9e00
r-m-hufford <ryan@rmhufford.com>
2021-04-25 15:10:12 -0400

    user MVC

--- src/main/java/kelly/ryan/xiong/chatbot/models/User.java
+++ src/main/java/kelly/ryan/xiong/chatbot/models/User.java
@@ -14,8 +14,54 @@ public class User {
     private String userName;
     private String password;
     private String email;
-//    private List<Channel> channelList;
+    //private List<Channel> channelList;
 
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+/*    public List<Channel> getChannelList() {
+        return channelList;
+    }
+
+    public void setChannelList(List<Channel> channelList) {
+        this.channelList = channelList;
+    }*/
 
 
 }
