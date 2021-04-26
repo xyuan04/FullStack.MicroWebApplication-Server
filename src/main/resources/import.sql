d6558cdc210becb2584e91ae8084b416b4ee307e
Xiong Yuan <78838190+xyuan04@users.noreply.github.com>
2021-04-25 22:27:00 -0400

    made sql queries for dummy data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,23 @@
+TRUNCATE TABLE CHANNEL;
+
+INSERT INTO CHANNEL (NAME)
+VALUES ('ANIME'),
+       ('SPORTS'),
+       ('MOVIES'),
+       ('ZIPCODE'),
+       ('ANIMALS'),
+       ('WALLSTREETBETS'),
+       ('CARS'),
+       ('SHOPPING'),
+       ('GAMES');
+
+TRUNCATE TABLE DIRECT_MESSAGE;
+
+INSERT INTO DIRECT_MESSAGE (NAME)
+VALUES ('KELLY'),
+       ('RYAN'),
+       ('XIONG'),
+       ('YOUNGER'),
+       ('DOLIO'),
+       ('DAN'),
+       ('DESA');
---
+++ target/classes/import.sql
@@ -0,0 +1,13 @@
+INSERT INTO CHANNEL (NAME)
+VALUES ('ANIME'),
+       ('SPORTS'),
+       ('MOVIES'),
+       ('ZIPCODE'),
+       ('ANIMALS'),
+       ('WALLSTREETBETS'),
+       ('CARS'),
+       ('SHOPPING'),
+       ('GAMES');
+
+
+INSERT INTO DIRECT
