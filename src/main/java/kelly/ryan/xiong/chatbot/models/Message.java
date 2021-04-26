e61b2530462433d1e0d2e534ffa3e2d3d172bcc8
Xiong Yuan <78838190+xyuan04@users.noreply.github.com>
2021-04-25 20:52:19 -0400

    reverted

--- src/main/java/kelly/ryan/xiong/chatbot/models/Message.java
+++ src/main/java/kelly/ryan/xiong/chatbot/models/Message.java
@@ -1,6 +1,5 @@
 package kelly.ryan.xiong.chatbot.models;
 
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,7 +8,6 @@ import java.util.Objects;
 
 @Entity
 public class Message {
-
     @Id
     @GeneratedValue
     private Long msgId;
@@ -18,82 +16,62 @@ public class Message {
     private Long senderId;
     private Long channelId;
     private Long directMessageId;
-
     public Long getMsgId() {
         return msgId;
     }
-
     public void setMsgId(Long msgId) {
         this.msgId = msgId;
     }
-
     public String getMessageBody() {
         return messageBody;
     }
-
     public void setMessageBody(String messageBody) {
         this.messageBody = messageBody;
     }
-
-
-
     public Long getSenderId() {
         return senderId;
     }
-
     public void setSenderId(Long senderId) {
         this.senderId = senderId;
     }
-
-    public Long getRecipientId() {
-        return recipientId;
+    public Date getTimeStamp() {
+        return timeStamp;
     }
-
-    public void setRecipientId(Long recipientId) {
-        this.recipientId = recipientId;
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
     }
-
-
     public Long getChannelId() {
         return channelId;
     }
-
     public void setChannelId(Long channelId) {
         this.channelId = channelId;
     }
-
     public Long getDirectMessageId() {
         return directMessageId;
     }
-
     public void setDirectMessageId(Long directMessageId) {
         this.directMessageId = directMessageId;
     }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Message)) return false;
         Message message = (Message) o;
-        return Objects.equals(msgId, message.msgId) && Objects.equals(messageBody, message.messageBody) && Objects.equals(timeSent, message.timeSent) && Objects.equals(senderId, message.senderId) && Objects.equals(recipientId, message.recipientId) && Objects.equals(channelId, message.channelId) && Objects.equals(directMessageId, message.directMessageId);
+        return Objects.equals(msgId, message.msgId) && Objects.equals(messageBody, message.messageBody) && Objects.equals(timeStamp, message.timeStamp) && Objects.equals(senderId, message.senderId) && Objects.equals(channelId, message.channelId) && Objects.equals(directMessageId, message.directMessageId);
     }
-
     @Override
     public int hashCode() {
-        return Objects.hash(msgId, messageBody, timeSent, senderId, recipientId, channelId, directMessageId);
+        return Objects.hash(msgId, messageBody, timeStamp, senderId, channelId, directMessageId);
     }
-
     @Override
     public String toString() {
         return "Message{" +
                 "msgId=" + msgId +
                 ", messageBody='" + messageBody + '\'' +
-                ", timeSent=" + timeSent +
+                ", timeStamp=" + timeStamp +
                 ", senderId=" + senderId +
-                ", recipientId=" + recipientId +
                 ", channelId=" + channelId +
                 ", directMessageId=" + directMessageId +
                 '}';
     }
-
 }
