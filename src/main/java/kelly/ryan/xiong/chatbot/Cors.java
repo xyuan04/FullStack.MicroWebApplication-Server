97f5c05039d44dcdf77eab072e3ab5cb367cdcfb
Xiong Yuan <78838190+xyuan04@users.noreply.github.com>
2021-04-29 19:22:13 -0400

    added Cors, system.properties for heroku hosting

---
+++ src/main/java/kelly/ryan/xiong/chatbot/Cors.java
@@ -0,0 +1,20 @@
+package kelly.ryan.xiong.chatbot;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class Cors {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
