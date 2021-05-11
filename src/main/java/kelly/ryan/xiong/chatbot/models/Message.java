209b0e992b188d7d64acdbf8867090a9502f0102
Xiong Yuan <78838190+xyuan04@users.noreply.github.com>
2021-05-11 17:52:51 -0400

    cleaned up code

--- src/main/java/kelly/ryan/xiong/chatbot/models/Message.java
+++ src/main/java/kelly/ryan/xiong/chatbot/models/Message.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
-import java.sql.Date;
 import java.util.Objects;
 
 @Entity
