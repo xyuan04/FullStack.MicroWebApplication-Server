4f523e32742687eb7d402e2731eecd0226d50551
Xiong Yuan <78838190+xyuan04@users.noreply.github.com>
2021-05-11 17:53:21 -0400

    cleaned up code

--- src/main/java/kelly/ryan/xiong/chatbot/repositories/MessageRepository.java
+++ src/main/java/kelly/ryan/xiong/chatbot/repositories/MessageRepository.java
@@ -1,6 +1,5 @@
 package kelly.ryan.xiong.chatbot.repositories;
 
-
 import kelly.ryan.xiong.chatbot.models.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
