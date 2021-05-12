abf51e9548e30c5b2fe3f33105357683c595c32c
Ryan M Hufford <ryan@rmhufford.com>
2021-05-11 20:25:53 -0400

    auth

--- src/main/java/kelly/ryan/xiong/chatbot/repositories/ConsumerRepository.java
+++ src/main/java/kelly/ryan/xiong/chatbot/repositories/ConsumerRepository.java
@@ -2,8 +2,11 @@ package kelly.ryan.xiong.chatbot.repositories;
 
 import kelly.ryan.xiong.chatbot.models.Consumer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-@RestController
+@Repository
 public interface ConsumerRepository extends JpaRepository<Consumer,Long> {
+
+    Consumer findByUserNameAndPassword(String userName, String password);
 }
